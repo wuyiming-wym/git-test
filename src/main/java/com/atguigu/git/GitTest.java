6206d92dcebd025cb3a64cb596cee38de26e3d72
yiming <646745914@qq.com>
2023-11-18 09:55:44 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Denmark
+ * @Create: 2023/11/17 - 19:58
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+    }
+}
